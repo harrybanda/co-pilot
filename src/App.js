53f1d5cf1d1b1c50158088f80f5c0621521e7e32
Harry Banda <harrybanda7@gmail.com>
2020-09-05 02:24:33 +0200

    Added code formatting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Axios from "axios";
+import Formatter from "code-formatter";
 import "./App.css";
 
 require("dotenv").config();
 
 function App() {
+  const [code, setCode] = useState("");
+
   const getCode = async (val) => {
     try {
       const response = await Axios.get(process.env.REACT_APP_CODE_URL + val);
-      console.log(response.data.Items[0].code["S"]);
+      const content = response.data.Items[0].code["S"];
+      const formatted = Formatter(content, {
+        method: "js",
+      });
+      setCode(formatted);
     } catch (error) {
       console.error(error);
     }
   };
 
   const sendMessage = async () => {
-    const message = "insert a for of loop";
+    const message = "insert a try catch block";
     const headers = {
       headers: {
         Authorization: "Bearer " + process.env.REACT_APP_WIT_TOKEN,
@@ -36,7 +43,7 @@ function App() {
   sendMessage();
   return (
     <div className="App">
-      <p>yeet!</p>
+      <textarea rows="30" cols="50" value={code} />
     </div>
   );
 }
