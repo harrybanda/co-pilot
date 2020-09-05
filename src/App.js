3bd1fbd81b4b4170d702f32165db96236c9c372e
Harry Banda <harrybanda7@gmail.com>
2020-09-05 02:00:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Axios from "axios";
+import "./App.css";
+
+require("dotenv").config();
 
 function App() {
+  const getCode = async (val) => {
+    try {
+      const response = await Axios.get(process.env.REACT_APP_CODE_URL + val);
+      console.log(response.data.Items[0].code["S"]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const sendMessage = async () => {
+    const message = "insert a for of loop";
+    const headers = {
+      headers: {
+        Authorization: "Bearer " + process.env.REACT_APP_WIT_TOKEN,
+      },
+    };
+    try {
+      const response = await Axios.get(
+        process.env.REACT_APP_WIT_URL + message,
+        headers
+      );
+      let entity = response.data.entities["code:code"][0].body;
+      getCode(entity.replace(/\s/g, ""));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  sendMessage();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>yeet!</p>
     </div>
   );
 }
